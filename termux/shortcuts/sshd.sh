7cc579df00e95f9d9f12c6b86e266bdf5db3bbb3
mapperr <michelangeloperrotta@gmail.com>
2019-11-01 15:49:27 +0100

    Fix paths

--- termux/shortcuts/sshd.sh
+++ termux/shortcuts/sshd.sh
@@ -1,5 +1,5 @@
 sshd
-echo "connect with: ssh `whoami`@`bash $HOME/bin-utilities/get-my-ip.sh` -p 8022"
+echo "connect with: ssh `whoami`@`wwctl lan ip` -p 8022"
 echo "press enter to stop"
 read c
 killall sshd
