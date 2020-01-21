d1cfa5dc71cb49986e7653c8e83e5e14feafd87c
mapperr <michelangeloperrotta@gmail.com>
2020-01-21 14:41:37 +0100

    Add scriptdir for symlinks resolve

---
+++ default/bin/scriptdir.sh
@@ -0,0 +1,14 @@
+# taken from gradlew
+PRG="$0"
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+SAVED="`pwd`"
+cd "`dirname \"$PRG\"`/" >/dev/null
+SCRIPTDIR="`pwd -P`"
