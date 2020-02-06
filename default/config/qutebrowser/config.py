16e2e1dfc008d87f9b75788a639419a594ad0f43
mapperr <michelangeloperrotta@gmail.com>
2020-02-06 16:14:44 +0100

    Dunno

--- default/config/qutebrowser/config.py
+++ default/config/qutebrowser/config.py
@@ -6,14 +6,15 @@ import os
 # Firefox desktop user agent
 # c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'
 
+c.confirm_quit = ["downloads"]
 c.content.autoplay = False
-c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'
+c.content.headers.user_agent = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'
 c.content.javascript.can_open_tabs_automatically = True
 c.downloads.location.directory = os.path.expanduser('~/tmp/')
-c.search.incremental = False
 c.editor.command = ['st', '-t', 'scratchpad', '-e', 'nvim' , '{file}']
 c.hints.auto_follow_timeout = 500
 c.input.partial_timeout = 30000
+c.search.incremental = False
 c.tabs.background = True
 c.tabs.last_close = 'close'
 c.url.default_page = 'about:blank'
@@ -35,8 +36,10 @@ for site in js_whitelist:
 
 # styling
 font_size = '12pt'
-monospace_font_definition = f'{font_size} monospace'
+monospace_font_definition = f'{font_size}  "Terminus (TTF)", "xos4 Terminus", Terminus, Monospace, "DejaVu Sans  Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier,  "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 monospace_bold_font_definition = f'bold {monospace_font_definition}'
+c.fonts.default_size = font_size
+c.fonts.default_family = monospace_font_definition
 c.fonts.completion.category = monospace_bold_font_definition
 c.fonts.completion.entry = monospace_font_definition
 c.fonts.debug_console = monospace_font_definition
@@ -46,7 +49,6 @@ c.fonts.keyhint = monospace_font_definition
 c.fonts.messages.error = monospace_font_definition
 c.fonts.messages.info = monospace_font_definition
 c.fonts.messages.warning = monospace_font_definition
-c.fonts.monospace = '"Terminus (TTF)", "xos4 Terminus", Terminus, Monospace, "DejaVu Sans  Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier,  "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 c.fonts.prompts = monospace_font_definition
 c.fonts.statusbar = monospace_font_definition
 c.fonts.tabs = monospace_font_definition
